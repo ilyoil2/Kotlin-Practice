6c01e79b25ce8c54401c619ef99b7da4ffd50ece
tae.y <0211ilyoil@gmail.com>
2023-12-28 08:19:20 +0900

    🧩 :: item 삭제 api 추가

--- src/main/kotlin/com/example/kotlinpractice/domain/item/presentation/ItemController.kt
+++ src/main/kotlin/com/example/kotlinpractice/domain/item/presentation/ItemController.kt
@@ -3,14 +3,20 @@ package com.example.kotlinpractice.domain.item.presentation
 import com.example.kotlinpractice.domain.item.presentation.dto.request.CreateItemRequest
 import com.example.kotlinpractice.domain.item.presentation.dto.response.ItemResponse
 import com.example.kotlinpractice.domain.item.service.CreateItemService
-import com.example.kotlinpractice.domain.item.service.GetItemService
+import com.example.kotlinpractice.domain.item.service.DeleteItemService
+import com.example.kotlinpractice.domain.item.service.QueryItemListService
+import com.example.kotlinpractice.domain.item.service.QueryItemService
 import org.springframework.web.bind.annotation.*
+import java.util.UUID
+import javax.validation.constraints.NotNull
 
 @RequestMapping("/item")
 @RestController
 class ItemController(
         private val createItemService: CreateItemService,
         private val queryItemListService: QueryItemListService,
+        private val deleteItemService: DeleteItemService,
+        private val queryItemService: QueryItemService
 ) {
     @PostMapping
     fun createItem(@RequestBody request: CreateItemRequest) {
@@ -25,6 +31,11 @@ class ItemController(
     @GetMapping("/{item-id}")
     fun queryItem(@PathVariable("item-id") @NotNull itemId: UUID): ItemResponse {
         return queryItemService.execute(itemId)
+    }
+
+    @DeleteMapping("/{item-id}")
+    fun deleteItem(@PathVariable("item-id") itemId : UUID) {
+        deleteItemService.execute(itemId)
     }
 
 }
---
+++ src/main/kotlin/com/example/kotlinpractice/domain/item/service/DeleteItemService.kt
@@ -0,0 +1,26 @@
+package com.example.kotlinpractice.domain.item.service
+
+import com.example.kotlinpractice.domain.item.domain.Item
+import com.example.kotlinpractice.domain.item.domain.repository.ItemRepository
+import com.example.kotlinpractice.domain.user.domain.User
+import com.example.kotlinpractice.domain.user.facade.UserFacade
+import io.lettuce.core.KillArgs.Builder.user
+import org.springframework.stereotype.Service
+import java.util.UUID
+import javax.transaction.Transactional
+
+@Service
+class DeleteItemService(
+        private val itemRepository: ItemRepository,
+        private val userFacade: UserFacade
+) {
+    @Transactional
+    fun execute(itemId : UUID) {
+        val user: User = userFacade.getCurrentUser()
+
+        val item: Item = itemRepository.findById(itemId)
+
+        itemRepository.delete(item)
+    }
+
+}
