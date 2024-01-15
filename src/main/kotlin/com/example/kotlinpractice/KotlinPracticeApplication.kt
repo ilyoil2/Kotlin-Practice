a0e131517e5994f5b0932e0bade34cdf5ad04463
ilyoil2 <0211ilyoil@gmail.com>
2024-01-15 17:33:11 +0900

    🧩 :: scheduler 추가

--- src/main/kotlin/com/example/kotlinpractice/KotlinPracticeApplication.kt
+++ src/main/kotlin/com/example/kotlinpractice/KotlinPracticeApplication.kt
@@ -3,9 +3,11 @@ package com.example.kotlinpractice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableScheduling
 class KotlinPracticeApplication
 
 fun main(args: Array<String>) {
